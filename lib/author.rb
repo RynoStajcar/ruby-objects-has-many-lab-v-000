0d8d4d854fe3e19d923a382c3ae39901babcac33
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-22 19:36:12 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
   attr_accessor :name
-
+  @@post_count = 0
   def initialize(name)
     @name = name
     @posts = []
@@ -13,11 +13,15 @@ class Author
   def add_post(post)
     self.posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     self.add_post(post)
+  end
 
+  def self.post_count
+    @@post_count
   end
 end
