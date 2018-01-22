31775147c7d62be5f22adf1fccf43c4de129ecb2
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-22 19:21:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
 
   attr_accessor :name
-
+  
   def initialize(name)
     @name = name
     @songs = []
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.song_count
-    self.songs
+    self.songs.count
   end
 
 end
